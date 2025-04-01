91e75f5eaa30a36954f30c47ed16ee67ce186310
ernestoIJ <jribaneze@gmail.com>
2025-04-01 16:35:29 -0700

    installed library

--- Audio-Video Synchronization/index.ts
+++ Audio-Video Synchronization/index.ts
@@ -2,7 +2,7 @@ import * as ffmpeg from 'fluent-ffmpeg';
 import * as path from 'path';
 
 // Path to the .avi video file
-const videoPath = path.join('converted_data','v_BoxingSpeedBag_g01_c03.avi');
+const videoPath = path.join('converted_data','v_BoxingSpeedBag_g01_c03.mp4');
 
 // Use ffprobe to get video metadata, including duration
 ffmpeg.ffprobe(videoPath, (err, metadata) => {
